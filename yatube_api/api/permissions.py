195ae2658f5757502e89e7fc394845a6d7776cbf
Denis <bochkarevdenis1995@gmail.com>
2024-10-24 15:13:01 +0300

    change docstring

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,8 +5,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """Разрешает редактировать и удалять объект только его автору."""
 
     def has_object_permission(self, request, view, obj):
-        # Разрешить методы GET, HEAD, OPTIONS для любого пользователя
-        # Разрешить редактирование и удаление только автору объекта
+        """
+        Проверяет, имеет ли пользователь право доступа к объекту.
+
+        Разрешает методы GET, HEAD, OPTIONS для любого пользователя.
+        Разрешает редактирование и удаление только автору объекта.
+        """
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
